f785dd2e730ce2884244bd855fb7a680283e803e
Mark Yeh <yelexin@hotmail.com>
2018-12-13 00:38:02 +0800

    feat: Authing koa2 middleware

---
+++ index.js
@@ -0,0 +1,29 @@
+const Authing = require('authing-js-sdk');
+
+let validAuth = null;
+
+module.exports = function (options) {
+    return async function (ctx, next) {
+        if (!validAuth) {
+            try {
+                validAuth = await new Authing({
+                    clientId: options.clientId,
+                    secret: options.secret
+                });
+                ctx.request.authing = validAuth;
+                try {
+                    await next()
+                } catch (e) {
+                    console.log(e)
+                }
+            } catch (e) {
+                validAuth = null;
+                console.log('koa-authing 中间件 - clientId 和 secret 验证失败');
+                console.log(e)
+            }
+        } else {
+            ctx.request.authing = validAuth;
+            await next();
+        }
+    };
+};
